040c1c58a0f612f4ae6b4bda24b0278475fbf1e6
devAYZ <ayokunle.fatokimi@decagon.dev>
2021-02-28 12:05:53 +0100

    add views to launch screen

--- devAYZ-TodoList/devAYZ-TodoList/TodoListViewController.swift
+++ devAYZ-TodoList/devAYZ-TodoList/TodoListViewController.swift
@@ -12,7 +12,7 @@ class TodoListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemOrange
     }
 
 
